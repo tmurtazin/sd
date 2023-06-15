48e2c229af19e2011a0b63ba90785682737f0c2a
Timur Murtazin <tmurtazin@yandex.ru>
2023-06-15 13:51:18 +0300

    switch to api gateway endpoint

--- curl.sh
+++ curl.sh
@@ -3,4 +3,4 @@
 curl -X POST \
   -H "Content-Type: application/json"  \
   -d @input.json \
-  http://ec2-18-210-153-29.compute-1.amazonaws.com:8000/v2/models/StableDiffusion_1_5/infer
+  https://vujouffcu8.execute-api.us-east-1.amazonaws.com/prod/v2/models/StableDiffusion_1_5/infer
